f16c51866d4d87d3c562e5b02fbc38178c5afdb4
mAbdulA05 <149349181+mAbdulA05@users.noreply.github.com>
2024-11-10 05:05:36 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,55 +1,60 @@
-const CACHE_NAME = 'v1';
-const URLS_TO_CACHE = [
+const staticAssets = [
+    '/',
     "/index.html",
+    "/offline.html", // Halaman fallback ketika offline
     "/css/style.css",
     "/img/profile.jpg",
     "/js/main.js",
-    "/img/favicon.ico",      // Jika favicon diperlukan
-    "/lib/slick/slick.css", // Jika Anda ingin meng-cache slick.css
-    "/lib/slick/slick-theme.css", // Jika Anda ingin meng-cache slick-theme.css
-    "/lib/lightbox/css/lightbox.min.css", // Jika Anda ingin meng-cache lightbox.min.css
-    "/lib/easing/easing.min.js", // Jika Anda ingin meng-cache easing.min.js
-    "/lib/slick/slick.min.js", // Jika Anda ingin meng-cache slick.min.js
-    "/lib/typed/typed.min.js", // Jika Anda ingin meng-cache typed.min.js
-    "/lib/waypoints/waypoints.min.js", // Jika Anda ingin meng-cache waypoints.min.js
-    "/lib/isotope/isotope.pkgd.min.js", // Jika Anda ingin meng-cache isotope.pkgd.min.js
-    "/lib/lightbox/js/lightbox.min.js", // Jika Anda ingin meng-cache lightbox.min.js
-];
+    "/img/favicon.ico",      
+    "/lib/slick/slick.css",
+    "/lib/slick/slick-theme.css",
+    "/lib/lightbox/css/lightbox.min.css",
+    "/lib/easing/easing.min.js",
+    "/lib/slick/slick.min.js",
+    "/lib/typed/typed.min.js",
+    "/lib/waypoints/waypoints.min.js",
+    "/lib/isotope/isotope.pkgd.min.js",
+    "/lib/lightbox/js/lightbox.min.js",
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Caching files');
-                return cache.addAll(URLS_TO_CACHE);
-            })
-    );
-});
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                // Cache hit - return response
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
-});
-
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
+  ];
+  
+  const STATIC_CACHE_NAME = 'static-data';
+  const DYNAMIC_CACHE_NAME = 'dynamic-data';
+  
+  self.addEventListener('install', async event => {
+    //self.skipWaiting();
+    const cache = await caches.open(STATIC_CACHE_NAME);
+    console.log('install');
+    cache.addAll(staticAssets);
+  });
+  
+  self.addEventListener('activate', e => {
+    console.log('activate');
+    return self.clients.claim();
+  });
+  
+  
+  self.addEventListener('fetch', event => {
+    const { request } = event;
+    event.respondWith(cacheData(request));
+  });
+  
+  async function cacheData(request) {
+    const cashedRequest = await caches.match(request);
+    if (staticAssets.some(sa => request.url.indexOf(sa) >= 0) || request.headers.get('accept').includes('text/html')) {
+      return cashedRequest || await caches.match('/offline.html') || networkFirst(request);
+    }
+    return cashedRequest || networkFirst(request);
+  }
+  
+  async function networkFirst(request) {
+    const cache = await caches.open(DYNAMIC_CACHE_NAME);
+    try {
+      const response = await fetch(request);
+      cache.put(request, response.clone());
+      return response;
+    } catch (error) {
+      return await cache.match(request);
+    }
+  }
